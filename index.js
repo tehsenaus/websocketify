1430c1df7be0e86e41fed940b343e75b0887a918
Sean Micklethwaite <sean@sdmworld.co.uk>
2017-02-16 00:27:25 +0000

    Fix #5, #4, #3

--- index.js
+++ index.js
@@ -30,70 +30,107 @@ var server = require('http').createServer(function (req, res) {
 });
 
 if ( program.args.length > 0 ) {
-	var numClients = 0, sharedProc;
+	var sharedProc;
 	var wss = new WebSocket.Server({ server: server });
 
 	wss.on('connection', function (ws) {
 		var proc;
 
-		numClients++;
+		if ( !sharedProc || sharedProc.isDead() ) {
+			console.log('websocketify: client connected: starting child process:',
+				program.args.join(' '));
 
-		if ( !program.singleton || numClients === 1 ) {
-			console.log('websocketify: starting child process:', program.args.join(' '));
-
-			proc = childProcess.spawn(program.args[0], program.args.slice(1), {
-				env: process.env
-			});
+			proc = createChildProcess();
 
 			if ( program.singleton ) {
 				sharedProc = proc;
 			}
 		} else {
+			console.log('websocketify: client connected: using existing child process');
 			proc = sharedProc;
 		}
 
-		var onData = function (data) {
-			var lines = data.toString('utf8').split('\n');
-			lines.slice(0, lines.length-1).forEach(function (line) {
-				ws.send(line);
-			});
-		}
-		proc.stdout.on('data', onData);
-
-		var onError = function (data) {
-			console.error('stderr:', data.toString('utf8'));
-		}
-		proc.stderr.on('data', onError);
-
-		var onExit = function (code) {
-			ws.close(code ? 1001 : 1000, '' + code);
-		}
-		proc.on('exit', onExit);
-
-		var onError = function (e) {
-			ws.close(1011, ''+e);
-		}
-		proc.on('error', onError);
+		proc.addSocket(ws);
 
 		ws.on('message', function (message) {
-			proc.stdin.write(message + '\n');
+			proc.process.stdin.write(message + '\n');
 		});
 		ws.on('close', function () {
-			numClients--;
-
-			if ( !program.singleton || numClients === 0 ) {
-				console.log('websocketify: stopping child process');
-				proc.kill();
+			if ( proc.removeSocket(ws) && sharedProc === proc ) {
 				sharedProc = null;
-			} else {
-				proc.stdout.removeListener('data', onData);
-				proc.stderr.removeListener('data', onError);
-				proc.removeListener('exit', onExit);
-				proc.removeListener('error', onError);
 			}
 		});
-
 	});
 }
 
-server.listen(parseInt(program.port || 8080), process.address || '0.0.0.0');
+var port = parseInt(program.port || 8080),
+	bindAddress = process.address || '0.0.0.0';
+server.listen(port, bindAddress);
+console.log('websocketify: listening on %s:%s', port, bindAddress);
+
+function createChildProcess() {
+	var proc = childProcess.spawn(program.args[0], program.args.slice(1), {
+		env: process.env
+	});
+
+	var sockets = [];
+	var dead = false;
+
+	proc.stdout.on('data', function (data) {
+		var lines = data.toString('utf8').split('\n');
+		lines.slice(0, lines.length-1).forEach(function (line) {
+			sockets.forEach(function (ws) {
+				ws.send(line);
+			});
+		});
+	});
+
+	proc.stderr.on('data', function (data) {
+		console.error('stderr:', data.toString('utf8'));
+	});
+
+	proc.on('exit', function (code) {
+		console.log('websocketify: child process exited with code %d', code);
+		dead = true;
+		sockets.forEach(function (ws) {
+			ws.close(code ? 1001 : 1000, '' + code);
+		});
+	});
+
+	proc.on('error', function (e) {
+		console.log('websocketify: child process error:', e);
+		dead = true;
+		sockets.forEach(function (ws) {
+			ws.close(1011, ''+e);
+		});
+	});
+
+	return {
+		process: proc,
+
+		isDead: function () {
+			return dead;
+		},
+
+		addSocket: function (ws) {
+			sockets.push(ws);
+		},
+
+		removeSocket: function (ws) {
+			sockets = sockets.filter(function (s) {
+				return s !== ws;
+			});
+
+			console.log('websocketify: socket disconnected, %d remain', sockets.length);
+
+			if ( sockets.length ) return false;
+
+			if ( !dead ) {
+				console.log('websocketify: stopping child process');
+				proc.kill();
+			}
+
+			return true;
+		}
+	}
+}
